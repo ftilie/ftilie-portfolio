5aa1565c44e9744935f585e05eb2c8d273eba065
ftilie <florintilie@gmail.com>
2025-03-09 18:13:25 +0200

    fix: fix firebase deployment

--- src/pages/ExperiencePage/ExperiencePage.tsx
+++ src/pages/ExperiencePage/ExperiencePage.tsx
@@ -1,15 +1,9 @@
-import { Box, Grid, Stack, Typography, useMediaQuery, useTheme } from "@mui/material";
-import { DarkThemeGrayAccentColor, WhiteBackgroundColor } from "~/utils/Theme";
-import { HEADER_HEIGHT, RESUME_PATH, SIDE_PADDING } from "~/utils/Constants";
+import { Stack, useMediaQuery, useTheme } from "@mui/material";
+import { HEADER_HEIGHT, RESUME_PATH } from "~/utils/Constants";
 import PDFViewer from "./PDFViewer/PDFViewer";
-import SideMenu from "~/components/SideMenu/SideMenu";
-const MIN_SKILL_CARD_WIDTH = 350;
-const MAX_SKILL_CARD_WIDTH = 500;
-const isMobile = /Mobi|Android|iPhone/i.test(navigator.userAgent);
 
 const ExperiencePage = (): JSX.Element => {
     const theme = useTheme();
-    const isSmScreen = useMediaQuery(theme.breakpoints.down("sm"));
     const isXlScreen = useMediaQuery(theme.breakpoints.up("xl"));
 
     return (
--- src/pages/ExperiencePage/PDFViewer/PDFViewer.tsx
+++ src/pages/ExperiencePage/PDFViewer/PDFViewer.tsx
@@ -1,6 +1,4 @@
 import { Viewer } from "@react-pdf-viewer/core";
-import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout";
-import { themePlugin } from "@react-pdf-viewer/theme";
 import { zoomPlugin } from "@react-pdf-viewer/zoom";
 import "@react-pdf-viewer/core/lib/styles/index.css";
 import "@react-pdf-viewer/default-layout/lib/styles/index.css";
